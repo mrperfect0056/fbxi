8ced4b22c5dbc7a64bc90a192f46c9134748c0de
Mr.Devil <87979690+mrperfect0056@users.noreply.github.com>
2021-10-26 22:27:28 +0600

    Add files via upload

---
+++ deta.py
@@ -0,0 +1,24 @@
+import os,sys,random
+
+os.system('clear')
+
+logo2 = """
+\x1b[1;96m
+| $$__  +8
+| $$  \ $+8
+| $
+| $$_+8
+| $$  \ 
+| mmmmm
+|_______/  \_______/   \___/   \_______/
+                                        """
+logo3 = '\x1b[0;35m \n      \x1b[0;33\n       \x1b[101m\x1b[37;>
+
+print logo2 + logo3
+nmbr = str(random.randint(11111, 99999))
+be = '\x1b[1;93mDEVIL-HACKED'
+logo1 = """ | """
+k = str('500000')
+c = str('102030')
+for n in range(60):
+    print be +  logo1 +k+nmbr + logo1 + c
